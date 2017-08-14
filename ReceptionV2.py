a74993f63c8f7b6e6f9cde6ab168ea0907f4f862
valuthy2 <valuthyk@gmail.com>
2017-08-14 18:28:30 +0200

    Add files via upload

--- ReceptionV2.py
+++ ReceptionV2.py
@@ -1,16 +1,34 @@
 import socket
 from os import getcwd,chdir
-from fonctionPratiques import *
+from myLib import *
 from sys import getsizeof
 from time import time
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-ip=input('IP:')
+loadThread=loadingThread()
+broadcast=-1
+while(broadcast==-1):
+	broadcast=getBroadcast()
+
+if(broadcast=="127.0.0.1"):
+	log("IP is set to localhost")
+	ip=broadcast
+else:
+	loadThread.start()
+	res=my_nmap("-F",broadcast+"*")
+	nTab, ipTab=getIps(res)
+	loadThread.running=False #loadThread checks every loop if running is True, if not, stops.
+	loadThread.join()
+	for a,ip in enumerate(ipTab):
+		print("[{}]{} : {}".format(a+1,nTab[a],ip))
+	selectedIP=int(input("Chose an IP: "))-1
+	ip=ipTab[selectedIP]
+
 port=int(input("PORT:"))
 client.connect((ip,port))
 print('[*]Connected')
-fichier=bytes()
+fichier=bytes()# Where received data will be stored
 recu=client.recv(1024).decode("utf-8")
-taille,ext=recu.split(',')
+taille,ext=recu.split(',')#recu contains the length of file and file extension, separated by a comma
 taille=int(taille)
 tailleRecu=0
 print("The length of {} is {} bytes\n[*]Receiving...".format(ext,taille))
@@ -18,7 +36,7 @@ stop=False
 debut=time()
 recCount=0
 while(not(stop)):
-	recu=client.recv(taille)
+	recu=client.recv(1024*1024)
 	tailleRecu=getsizeof(fichier)
 	recCount+=1
 	if(recu==b'stop' or recu==b''):
@@ -35,9 +53,9 @@ if(Rep1_0()==True):
 	print(fichier)
 else:
 	print("[*]Not printing")
-print("Save reveived file?")
+print("Save received file?")
 if(Rep1_0()==True):
-	print("Enter path for saving(enter # if you want to save {})".format(getcwd()))
+	print("Enter path for saving(# = {})".format(getcwd()))
 	chemin=input()
 	if (not(chemin=="#")):
 		chdir(chemin)
@@ -45,6 +63,6 @@ if(Rep1_0()==True):
 	nomFichier=input()+ext
 	with open(nomFichier,"wb") as file:
 		file.write(fichier)
-		print("[*]Saving done")
+	print("[*]Saving done")
 print("[*]Bye !")
 client.close()
---
+++ myLib.py
@@ -0,0 +1,105 @@
+import netifaces
+import subprocess
+from threading import Thread
+from time import sleep
+def Rep1_0():
+	loopInput=True
+	while(loopInput):
+		reponse=input()
+		if(reponse.upper()=="Y"):
+			loopInput=False
+			return True
+		elif(reponse.upper()=="N"):
+			loopInput=False
+			return False
+		else:
+			print("enter 'y' for yes or 'n' for 'no'")
+
+def getBroadcast():
+	"""
+	This function finds the broadcast of the user's interface.
+	An example of broadcast : 192.168.43.255 #In a broadcast,  255 can be replaced by 2-254, not 1 because it's usually the router
+	"""
+	interfaces=netifaces.interfaces()
+	log("Chose a network interface please.",True)
+	for a,i in enumerate(interfaces): #We print the list of interfaces
+		print("[{}]:{}".format(a,i))
+	interNo=int(input())
+	if(interfaces[interNo]=="lo"): #if the user choses the loopback interface, we assume that ip=localhost/127.0.0.1
+		return "127.0.0.1"
+	interInfos=netifaces.ifaddresses(interfaces[interNo])
+	try: #We use a try block because some interfaces don't have the wanted ipv4Infos
+		ipv4Infos=interInfos[netifaces.AF_INET]
+		return ipv4Infos[0]["broadcast"][:11] # [:11] because we just need the 3 first digits.
+	except:
+		log("This interface can't be used.",True)
+		return -1
+def log(str,returnLine=True):
+	finalStr="[*]"+str
+	if(returnLine):
+		finalStr+="\n"
+	print(finalStr,end="")
+
+def my_nmap(args,ipRangeStr,prCmd=False): # Simply excecutes a command and returns the output
+	"""
+	This just runs nmap with some args.
+	RETURN : a String containing nmap's output
+	"""
+	cmd="nmap "+args+" "+ipRangeStr
+	if(prCmd):
+		print(cmd)
+	resCmd=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+	res, placeholder=resCmd.communicate()
+	res=res.decode()
+	return res
+
+def getIps(res): # Here we search for detected IPs on the local network in a loop
+	"""
+	This function gets the ip and host name information from the res String var.
+	res contains the output of a nmap command.
+	nmap is launched by "my_nmap" function.
+	RETURN : 2 tables, the 1st contains hostNames and the 2nd the IPs.
+	"""
+	resTab=res.split("\n")
+	ChoiceTab=[]
+	ipTab=[]
+	for l in resTab:
+		if(l.startswith("Nmap scan report for")):
+			name=l[21:]
+			nameCpy=name
+			try: #We handle an IndexError here *[*read down*]*
+				ip=name.split(" ")[1]# There will be an IndexError because ip will not have " " in it and split will return a 1 length table
+				name=name.split(" ")[0] #ip will not have " " if nmap didn't find the name because nmap prints the hostname+" "+ip
+				ChoiceTab.append(name)
+				ip=ip[1:len(ip)-1]
+				separatedIp=ip.split(".")
+				ip=""
+				for part in separatedIp:
+					ip+="."+part
+					ip=ip[1:]
+				ipTab.append(ip)
+			except IndexError:
+				ipTab.append(nameCpy)
+				ChoiceTab.append("UNKNOWN")
+
+
+	return ChoiceTab,ipTab
+
+def getPort(ip,portRange):
+	res=my_nmap("-p "+portRange,ip,True)
+	print(res)
+
+class loadingThread(Thread):
+	def __init__(self):
+		Thread.__init__(self)
+		self.loadIndex=0
+		self.loadingText=["-","\\","|","/"]
+		self.running=True
+	def run(self):
+		while(self.running):
+			print("\rSearching... {}".format(self.loadingText[self.loadIndex]),end="")
+			self.loadIndex+=1
+			if(self.loadIndex>3):
+				self.loadIndex=0
+			sleep(0.5)
+		print("")
